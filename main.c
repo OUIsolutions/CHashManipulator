7eca3719dc90a8d4776f0c6d1e44f386ce897da3
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-09-16 16:48:35 -0300

    att

--- main.c
+++ main.c
@@ -7,18 +7,6 @@ CHashArray  * create_array(){
 
     CHashArray * element = newCHashArray();
 
-    long size = CHash_get_size(element);
-    for(int i = 0; i < size; i ++){
-
-        CHashArrayItem *  current = CHash_get_from_index(element,size);
-        int type = CHash_get_type(current);
-        if(type == CHASH_STRING){
-            char *value = CHash_get_string(current);
-            
-        }
-
-    }
-
     return element;
 }
 
