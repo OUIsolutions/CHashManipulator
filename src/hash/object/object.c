0e53628a0d13d4c0b81db7d14190ba58218e7a33
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-09-17 03:03:38 -0300

    at

---
+++ src/hash/object/object.c
@@ -0,0 +1,5 @@
+
+
+CHashObject* newCHashObject(){
+
+}
--- src/hash/object/object.h
+++ src/hash/object/object.h
@@ -6,8 +6,8 @@ CHashObject* newCHashObject();
 
 int CHashObject_set(CHashObject * object,const char *key, CHash *element);
 
-CHashAny * privateCHashObject_get_by_key(CHashObject * object, const char *key);
+CHash * privateCHashObject_get_by_key(CHashObject * object, const char *key);
 
-CHashAny * CHashObject_get_by_index(CHashObject * object, const char *key);
+CHash * CHashObject_get_by_index(CHashObject * object, const char *key);
 
-CHashAny * CHashObject_get(CHashObject * object, const char *key);
+CHash * CHashObject_get(CHashObject * object, const char *key);
