2c1bc74e69f4cf66eb8cdaa71f2fed924c97f814
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-09-19 15:25:08 -0300

    at

--- main.c
+++ main.c
@@ -15,24 +15,10 @@ CHashObject *create (){
     );
 }
 
-
 int main(){
 
-    CHashObject *t = create();
-    //printf("t: %ld\n",t);
-    CHash *e1  = CHashObject_get(t,"a");
+    CHashObject *t = newCHashLong(20);
     
-
-    CHash *age = CHashObject_get(e1,"age");
-
-
-    CHash_raise_error(age,10,"value: #value# at #path# its not correct",NULL);
-    //CHash_print(age);
-    printf("error %d\n",sizeof(CHash));
-
-    if( Chash_errors(t)){
-
-    }
     CHash_free(t);
 
 }
