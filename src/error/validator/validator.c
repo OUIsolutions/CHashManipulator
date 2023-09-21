8e49b7ec38ec06a381c80d4c4539cc5a46d8e434
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-09-20 23:05:24 -0300

    at

--- src/error/validator/validator.c
+++ src/error/validator/validator.c
@@ -1,15 +1,25 @@
-
-int CHash_ensure_double(CHash *element){
+int private_chash_check_type(CHash *element, unsigned short  expected_type,char *expected_type_str){
     if(Chash_errors(element)){
         return 1;
     }
 
-    if(element->private_type != CHASH_DOUBLE){
+    if(element->private_type != expected_type){
         CHash_raise_error(element,
-         CHASH_WRONG_TYPE,
-         "value: #value# of at #path# its not a double ", NULL
-         );
+                          CHASH_WRONG_TYPE,
+                          "value: #value# of at #path# its not #type# ",
+                          newCHashObject("type", newCHashString(expected_type_str))
+                          );
         return 1;
     }
     return 0;
 }
+
+int CHash_ensure_double(CHash *element){
+    return private_chash_check_type(element,CHASH_DOUBLE,"double");
+}
+
+int CHash_ensure_string(CHash *element){
+    return private_chash_check_type(element,CHASH_STRING,"string");
+}
+
+
--- src/error/validator/validator.h
+++ src/error/validator/validator.h
@@ -1,3 +1,6 @@
 
+int private_chash_check_type(CHash *element, unsigned short  expected_type,char *expected_type_str);
 
 int CHash_ensure_double(CHash *element);
+
+int CHash_ensure_string(CHash *element);
