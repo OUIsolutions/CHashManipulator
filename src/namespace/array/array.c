6e207860722e8c325a78a2e32bf91282882ccd44
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-10-02 02:57:43 -0300

    at

--- src/namespace/array/array.c
+++ src/namespace/array/array.c
@@ -6,5 +6,11 @@ CHashArrayModule newCHashArrayModule(){
     self.append_once = CHashArray_append_once;
     self.delete = CHashArray_delete;
     self.get = CHashArray_get;
+    self.getArray = CHashArray_getArray;
+    self.getObject = CHashArray_getObject;
+    self.getString = CHashArray_getString;
+    self.getDouble = CHashArray_getDouble;
+    self.getLong = CHashArray_getLong;
+    self.getBool = CHashArray_getBool;
     return self;
 }
--- src/namespace/array/array.h
+++ src/namespace/array/array.h
@@ -6,7 +6,12 @@ typedef struct CHashArrayModule{
     void (*append_once)(CHashArray *self, CHash *element);
     void  (*delete)(CHashArray *self, long index);
     CHash * (*get)(CHashArray *self, long position);
-
+    CHashArray * (*getArray)(CHashObject * self, long index);
+    CHashObject * (*getObject)(CHashObject * self, long index);
+    long (*getLong)(CHashObject * self, long index);
+    double (*getDouble)(CHashObject * self, long index);
+    bool (*getBool)(CHashObject * self, long index);
+    char  * (*getString)(CHashObject * self, long index);
 
 }CHashArrayModule;
 
