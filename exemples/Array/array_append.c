8bb029e910af7c3cdd1810857d689108ff4bf198
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-10-02 18:55:14 -0300

    at

---
+++ exemples/Array/array_append.c
@@ -0,0 +1,44 @@
+
+#include "CHashManipulator.h"
+
+
+
+CHashNamespace hash;
+CHashObjectModule  obj;
+CHashArrayModule  array;
+CHashValidatorModule validator;
+
+
+CHashObject *create (){
+    return newCHashArray(
+            hash.newString("aaa"),
+            hash.newLong(26),
+            hash.newDouble(20),
+            hash.newBool(true)
+    );
+}
+
+int main(){
+    hash = newCHashNamespace();
+    obj = hash.object;
+    array  = hash.array;
+    validator = hash.validator;
+    CHashArray *element = create();
+
+    CHashArray_append(element,
+       hash.newString("b"),
+       hash.newString("c")
+    );
+
+    if(!hash.errors(element)){
+        hash.print(element);
+
+    }
+    else{
+        printf("%s",hash.get_error_menssage(element));
+
+    }
+
+    hash.free(element);
+
+}
