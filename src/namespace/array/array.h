85a1df5a0d86f56d203c378ea452953b58f31f3e
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-10-02 18:58:33 -0300

    at

--- src/namespace/array/array.h
+++ src/namespace/array/array.h
@@ -4,16 +4,16 @@ typedef struct CHashArrayModule{
 
     CHashArray  *(*newArrayEmpty)();
     void (*append_once)(CHashArray *self, CHash *element);
-    void  (*delete)(CHashArray *self, long index);
-    CHash * (*get)(CHashArray *self, long position);
-    short (*get_type)(CHashArray *self, long index);
+    void  (*delete)(CHashArrayOrObject *self, long index);
+    CHash * (*get)(CHashArrayOrObject *self, long position);
+    short (*get_type)(CHashArrayOrObject *self, long index);
 
-    CHashArray * (*getArray)(CHashObject * self, long index);
-    CHashObject * (*getObject)(CHashObject * self, long index);
-    long (*getLong)(CHashObject * self, long index);
-    double (*getDouble)(CHashObject * self, long index);
-    bool (*getBool)(CHashObject * self, long index);
-    char  * (*getString)(CHashObject * self, long index);
+    CHashArray * (*getArray)(CHashArrayOrObject * self, long index);
+    CHashObject * (*getObject)(CHashArrayOrObject * self, long index);
+    long (*getLong)(CHashArrayOrObject * self, long index);
+    double (*getDouble)(CHashArrayOrObject * self, long index);
+    bool (*getBool)(CHashArrayOrObject * self, long index);
+    char  * (*getString)(CHashArrayOrObject * self, long index);
 
 }CHashArrayModule;
 
