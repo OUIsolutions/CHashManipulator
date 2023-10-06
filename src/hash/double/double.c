cdfe8d06b60fb31d8e44f65d9f3dc60b00d30962
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-10-06 02:48:36 -0300

    starting removing long

--- src/hash/double/double.c
+++ src/hash/double/double.c
@@ -5,10 +5,10 @@ double CHash_toDouble(CHash *element){
     if(CHash_ensure_Double(element)){
         return -1;
     }
-
     return element->private_value_double;
 
 }
+
 
 CHash * newCHashDouble(double value){
     CHash * self =  privatenewChash_raw();
