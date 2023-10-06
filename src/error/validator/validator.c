8a521419645244212884ac87b2b0e521de18755e
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-10-06 02:43:14 -0300

    at

--- src/error/validator/validator.c
+++ src/error/validator/validator.c
@@ -22,25 +22,85 @@ int private_chash_check_type(CHash *element, unsigned short  expected_type){
 int CHash_ensure_Double(CHash *element){
     return private_chash_check_type(element,CHASH_DOUBLE);
 }
-
-int CHash_ensure_String(CHash *element){
-    return private_chash_check_type(element,CHASH_STRING);
+int CHash_ensure_Double_by_key(CHash *object,const char *key){
+    CHashObject *current = CHashObject_get(object,key);
+    return CHash_ensure_Double(current);
 }
+
+int CHash_ensure_Double_by_index(CHash *array,long index){
+    CHashArray *current = CHashArray_get(array,index);
+    return CHash_ensure_Double(current);
+}
+
+
+
 int CHash_ensure_Long(CHash *element){
     return private_chash_check_type(element,CHASH_LONG);
 
 }
+
+int CHash_ensure_long_by_key(CHash *object , const char *key){
+    CHashObject *current = CHashObject_get(object,key);
+    return CHash_ensure_Long(current);
+}
+
+int CHash_ensure_long_by_index(CHash *array , long index){
+    CHash *current = CHashArray_get(array,index);
+    return CHash_ensure_Long(current);
+}
+
 int CHash_ensure_Bool(CHash *element){
     return private_chash_check_type(element,CHASH_BOOL);
+
+}
+
+int CHash_ensure_Bool_by_key(CHash *object , const char *key){
+    CHashObject *current = CHashObject_get(object,key);
+    return CHash_ensure_Bool(current);
+}
+
+int CHash_ensure_Bool_by_index(CHash *array , long index){
+    CHash *current = CHashArray_get(array,index);
+    return CHash_ensure_Bool(current);
+}
+
+int CHash_ensure_String(CHash *element){
+    return private_chash_check_type(element,CHASH_STRING);
+}
+int CHash_ensure_String_by_key(CHash *object , const char *key){
+    CHashObject *current = CHashObject_get(object,key);
+    return CHash_ensure_String(current);
+}
+int CHash_ensure_String_by_index(CHash *array , long index){
+    CHash *current = CHashArray_get(array,index);
+    return CHash_ensure_String(current);
 }
 
 int CHash_ensure_Object(CHash *element){
     return private_chash_check_type(element,CHASH_OBJECT);
 }
+int CHash_ensure_Object_by_key(CHash *object , const char *key){
+    CHashObject *current = CHashObject_get(object,key);
+    return CHash_ensure_Object(current);
+}
+int CHash_ensure_Object_by_index(CHash *array , long index){
+    CHash *current = CHashArray_get(array,index);
+    return CHash_ensure_Object(current);
+}
 
 int CHash_ensure_Array(CHash *element){
     return private_chash_check_type(element,CHASH_ARRAY);
 }
+int CHash_ensure_Array_by_key(CHash *object , const char *key){
+    CHashObject *current = CHashObject_get(object,key);
+    return CHash_ensure_Array(current);
+}
+
+int CHash_ensure_Array_by_index(CHash *array , long index){
+    CHash *current = CHashArray_get(array,index);
+    return CHash_ensure_Array(current);
+}
+
 int privateCHash_ensureArrayOrObject(CHash *element){
     if(Chash_errors(element)){
         return 1;
--- src/error/validator/validator.h
+++ src/error/validator/validator.h
@@ -2,15 +2,30 @@
 int private_chash_check_type(CHash *element, unsigned short  expected_type);
 
 int CHash_ensure_Double(CHash *element);
+int CHash_ensure_Double_by_key(CHash *object,const char *key);
+int CHash_ensure_Double_by_index(CHash *array,long index);
+
+
 
 int CHash_ensure_Long(CHash *element);
+int CHash_ensure_long_by_key(CHash *object , const char *key);
+int CHash_ensure_long_by_index(CHash *array , long index);
+
 
 int CHash_ensure_Bool(CHash *element);
+int CHash_ensure_Bool_by_key(CHash *object , const char *key);
+int CHash_ensure_Bool_by_index(CHash *array , long index);
 
 int CHash_ensure_String(CHash *element);
+int CHash_ensure_String_by_key(CHash *object , const char *key);
+int CHash_ensure_String_by_index(CHash *array , long index);
 
 int CHash_ensure_Object(CHash *element);
+int CHash_ensure_Object_by_key(CHash *object , const char *key);
+int CHash_ensure_Object_by_index(CHash *array , long index);
 
 int CHash_ensure_Array(CHash *element);
+int CHash_ensure_Array_by_key(CHash *object , const char *key);
+int CHash_ensure_Array_by_index(CHash *array , long index);
 
 int privateCHash_ensureArrayOrObject(CHash *element);
