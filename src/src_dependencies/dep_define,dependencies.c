4e2a3edb157b55a2dcfd8930fd35a56722157ce3
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-03 06:50:31 -0300

    att

---
+++ src/src_dependencies/dep_define,dependencies.c
@@ -0,0 +1,9 @@
+
+#if !defined(CHASH_MANIPULATOR_MOCK_CTEXT_ENGINE_DEFINE) && defined(CHASH_MANIPULATOR_CTEXT_ENGINE_DECLARATED)
+#include "../../dependencies/CTextEngine.c"
+#endif
+
+#if !defined(CHASH_MANIPULATOR_MOCK_CJSON_DEFINE) && defined(CHASH_MANIPULATOR_CJSON_DECLARATED)
+#include "../../dependencies/cJSON.c"
+#endif
+
